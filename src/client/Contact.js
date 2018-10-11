dd3a4686d224481dd2e586ff2562d6590b8e7c2f
joycejoycejoyce <joycezhou757@gmail.com>
2018-10-11 06:47:13 -0400

    contact experience done

--- src/client/Contact.js
+++ src/client/Contact.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
+const Div =styled.div`
+    padding: 1% 17% 3% 17%;
+`;
+const Textarea = styled.textarea`
+    border-radius:5%;
+    background: transparent;
+    width:100%;
+`;
 class Contact extends Component{
+    constructor() {
+        super();
+        this.state = {
+          name: '',
+          email:'',
+          content:'',
+        };
+      }
     render(){
         return(
-            <div>
-                <div>
-                Contact
-                </div>
-            </div>
+            <Div>
+                <form action="https://formspree.io/sophiaciocca@gmail.com" method="POST">
+                <Grid container spacing={8}>
+                    <Grid item xs={12} sm={6}>
+                        <span>Name:</span>
+                        <TextField
+                            fullWidth
+                            id="outlined-full-width"
+                            class="name"
+                            value={this.state.name}
+                            variant="outlined"
+                            type="text"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                    <span>Email:</span>
+                    <TextField
+                            fullWidth
+                            id="outlined-full-width"
+                            class="email"
+                            value={this.state.email}
+                            variant="outlined"
+                            type="text"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                    <Textarea 
+                        id="content"
+                        rows="10" 
+                        cols="50" 
+                        maxLength={300}
+                        value={this.state.content}                      
+                        >
+                    </Textarea>
+                    </Grid>
+                    <Grid item xs={12}>
+                    <input type="submit" value="submit"/>
+                    </Grid>
+                    </Grid>
+                </form>
+                
+            </Div>
         )
     }
 }
--- src/client/Experience.js
+++ src/client/Experience.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import Grid from '@material-ui/core/Grid';
+
+const SkillWrapper = styled(Grid)`
+    position: relative;
+    text-align: center;
+`;
+const Blur = styled.div`
+    position: relative;
+`
+const Itext = styled.div`
+    position: absolute;
+`;
+const I = styled.i`
+    ${Blur}:hover &{
+        filter: blur(5px);
+    }
+`;
+
 
 class Experience extends Component{
     render(){
         return(
             <div>
-                <div>
-                Experience
-                </div>
+                <Grid container spacing={8}>
+                    <Grid item xs={12}>
+                        <h2>Skill</h2>
+                    </Grid>
+                    <SkillWrapper item xs={12} sm={4}>
+                        <Blur>
+                            <I class="fab fa-html5 fa-7x"></I>
+                        </Blur>
+                    </SkillWrapper>
+                    <SkillWrapper item xs={12} sm={4}>
+                        <i class="fab fa-css3-alt fa-7x"></i>
+                    </SkillWrapper>
+                    <SkillWrapper item xs={12} sm={4}>
+                        <i class="fab fa-js fa-7x"></i>
+                    </SkillWrapper>
+                    <SkillWrapper item xs={12} sm={4}>
+                        <i class="fab fa-react fa-7x"></i>
+                    </SkillWrapper>
+                    <SkillWrapper item xs={12} sm={4}>
+                    <i class="fab fa-php fa-7x"></i>
+                    </SkillWrapper>
+                    <SkillWrapper item xs={12} sm={4}>
+                    <i class="fab fa-linux fa-7x"></i>
+                    </SkillWrapper>
+                    <SkillWrapper item xs={12} sm={4}>
+                        <i class="fab fa-github fa-7x"></i>   
+                    </SkillWrapper>
+                </Grid>
             </div>
         )
     }
